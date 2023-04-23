8f95aa690b624a64aab0de54e5ce515bbffbe07d
carrie nye <cnye@chicagobooth.edu>
2023-04-23 19:52:12 +0000

    only 1 point away from finishing

---
+++ app/controllers/actors_controller.rb
@@ -0,0 +1,16 @@
+class ActorsController < ApplicationController
+  def index
+    @list_of_actors = Actor.all
+    render ({ :template => "actor_templates/index.html.erb"})
+  end
+
+  def actor_details
+    the_id = params.fetch("an_id")
+    @the_actor = Actor.where({ :id => the_id }).at(0)
+    @filmography = Character.where({ :actor_id => @the_actor.id })
+
+    #@the_director = Director.where({ :id => the_id }).at(0)
+    #@filmography_director = Movie.where({ :director_id => @the_director.id })
+    render ({ :template => "actor_templates/show.html.erb"})
+  end
+end
